efb6a13986770f1717c9387820c2a8ef58c30405
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-07 04:22:21 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song 
+  attr_accessor :artist, :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all.push(self)
+  
+  end 
+  
+  def self.all
+    @@all  
+  end
+  
+  def artist=(artist)
+    @artist = artist 
+  end 
+  
+  def artist 
+    @artist
+  end
+  
+  def artist_name 
+    if self.artist
+      self.artist.name
+    else 
+      nil 
+    end 
+  end 
+end
