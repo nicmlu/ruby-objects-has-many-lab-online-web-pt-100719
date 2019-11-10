944e7b3576515d77f92c4f830ea2b5f72125b680
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-10 15:51:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -45,5 +45,11 @@ class Author
   def add_post(post)
     post.author = self 
   end
+  
+  def add_post_by_title(post_title)
+    new_post = Post.new(post_title)
+    new_post.author == self 
+  
+  end 
 
 end 
