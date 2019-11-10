23305147e567db0b685f90b05d2dc21014c0fa10
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-10 15:06:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -37,11 +37,10 @@ class Artist
 
   def initialize(name)
     @name = name 
-    @songs = []
   end
   
   def songs 
-    @songs 
+    Song.all.select {|song| song.artist == self}  
   end 
 
 end 
