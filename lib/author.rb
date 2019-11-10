3586d59c07ee00e2c8ae642ecec677ce62555dc9
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-10 15:00:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,32 +1,32 @@
-class Author 
-  attr_accessor :name
-  @@all_authors = []
+# class Author 
+#   attr_accessor :name
+#   @@all_authors = []
   
-  def initialize(author_name)
-    @name = author_name
-    @@all_authors.push(self)
-  end
+#   def initialize(author_name)
+#     @name = author_name
+#     @@all_authors.push(self)
+#   end
   
-  def self.all_authors
-    @@all_authors
-  end
+#   def self.all_authors
+#     @@all_authors
+#   end
   
-  def posts
-    Post.all.select {|post| post.author == self}
-  end 
+#   def posts
+#     Post.all.select {|post| post.author == self}
+#   end 
   
-  def add_post(post)
-    post.author = self   
-  end
+#   def add_post(post)
+#     post.author = self   
+#   end
 
-  def add_post_by_title(post_title) 
-    post = Post.new(post_title)
-    add_post(post)
-  end 
+#   def add_post_by_title(post_title) 
+#     post = Post.new(post_title)
+#     add_post(post)
+#   end 
   
-  def self.post_count
-  #returns the total number of posts associated to the existing authors 
-    all_posts = self.all_authors.collect {|author| author.posts} 
-    all_posts.flatten.count
-  end
-end
+#   def self.post_count
+#   #returns the total number of posts associated to the existing authors 
+#     all_posts = self.all_authors.collect {|author| author.posts} 
+#     all_posts.flatten.count
+#   end
+# end
